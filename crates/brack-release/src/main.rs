764d8a79e017c3e87d7d1b0cf2ee0b55b82c3ace
Mutsuha Asada <me@momee.mt>
2024-11-08 19:15:42 +0900

    add: update version and rewrite Cargo.toml

--- crates/brack-release/src/main.rs
+++ crates/brack-release/src/main.rs
@@ -1,3 +1,80 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::read_to_string;
+use std::path::Path;
+use toml_edit::{DocumentMut, value};
+
+use anyhow::Result;
+use clap::{Parser, Subcommand, ValueEnum};
+
+mod semver;
+use crate::semver::SemVer;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(subcommand)]
+    sub_commands: SubCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum SubCommands {
+    Update { semver_kind: SemVerKind },
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+enum SemVerKind {
+    Major,
+    Minor,
+    Patch,
+}
+
+fn get_current_version<P: AsRef<Path>>(path: P) -> Result<SemVer> {
+    let file = read_to_string(path)?;
+    let mut lines = file.lines();
+    let version = SemVer::new_with_string(
+        lines
+            .next()
+            .ok_or_else(|| anyhow::anyhow!("No version found"))?,
+    )?;
+    Ok(version)
+}
+
+fn rewrite_version<P: AsRef<Path> + Copy>(path: P, version: &SemVer) -> Result<()> {
+    let version = version.to_string();
+    let toml_str = read_to_string(path)?;
+    let mut toml = toml_str.parse::<DocumentMut>()?;
+    toml["package"]["version"] = value(version);
+    let toml_str = toml.to_string();
+    std::fs::write(path, toml_str)?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    match args.sub_commands {
+        SubCommands::Update { semver_kind } => {
+            let current_version = get_current_version("VERSION")?;
+            println!("Current version: {}", current_version);
+            let next_version = match semver_kind {
+                SemVerKind::Major => current_version.next_major(),
+                SemVerKind::Minor => current_version.next_minor(),
+                SemVerKind::Patch => current_version.next_patch(),
+            };
+            println!("Next version: {}", next_version);
+            let cargo_toml_paths = [
+                "Cargo.toml",
+                "crates/brack-codegen/Cargo.toml",
+                "crates/brack-expander/Cargo.toml",
+                "crates/brack-language-server/Cargo.toml",
+                "crates/brack-parser/Cargo.toml",
+                "crates/brack-plugin/Cargo.toml",
+                "crates/brack-plugin/Cargo.toml",
+                "crates/brack-project-manager/Cargo.toml",
+                "crates/brack-tokenizer/Cargo.toml",
+                "crates/brack-transformer/Cargo.toml",
+            ];
+            for path in cargo_toml_paths.iter() {
+                rewrite_version(path, &next_version)?;
+            }
+        }
+    }
+    Ok(())
 }
